5e965ff66a61dc6481537f0cfbd68d9068c2a812
Felipe Borges <lipex360@gmail.com>
2021-11-15 19:07:53 -0300

    components started

---
+++ vue_components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello Vue</h1>
+  <ButtonStyled />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import ButtonStyled from '@/components/ButtonStyled.vue'
+
+export default defineComponent({
+  components: { ButtonStyled }
+})
+</script>
