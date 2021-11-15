efbf75c3d193bc23f51995d3e64fcb6911deb3af
Felipe Borges <lipex360@gmail.com>
2021-11-15 16:34:46 -0300

    directive v-bind

---
+++ vue_directives/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <a :href="products.url" :class="productClass">
+    {{ products.name }}
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      products: {
+        name: 'Camisa',
+        url: 'http://loja.com/camisa',
+        available: true
+      }
+    }
+  },
+  computed: {
+    productClass(): string {
+      return this.products.available ? 'success' : 'danger'
+    }
+  }
+})
+</script>
+
+<style scoped>
+.success {
+  color: green;
+}
+
+.danger {
+  color: red;
+}
+</style>
