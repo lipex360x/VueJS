1d9d835730cfcb517919690a6f0e83e119ac85b8
Felipe Borges <lipex360@gmail.com>
2021-11-15 12:30:59 -0300

    property methods

--- vue_cli/src/App.vue
+++ vue_cli/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>{{ fullName }}</h1>
+
+  <h2>{{ count }}</h2>
+  <button @click="increment">Add</button>
+  <button @click="decrease">Del</button>
+  <button @click="reset">reset</button>
 </template>
 
 <script lang="ts">
@@ -9,12 +14,26 @@ export default defineComponent({
   data() {
     return {
       name: 'Hello',
-      lastname: 'World'
+      lastname: 'World',
+      count: 0
     }
   },
+
   computed: {
     fullName(): string {
       return `${this.name} ${this.lastname}`
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrease() {
+      this.count--
+    },
+    reset() {
+      this.count = 0
     }
   }
 })
