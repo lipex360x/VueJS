1d5e9787b48b5dc9a9aa54516401bbd7746fa61a
Felipe Borges <lipex360@gmail.com>
2021-11-15 20:53:02 -0300

    vue_local updated

---
+++ vue_local/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ top }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      top: 'Logo'
+      }  
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
